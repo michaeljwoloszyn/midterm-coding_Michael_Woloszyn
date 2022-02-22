012c338d31d6878ed61d460c9b161a3a99c7b57f
991571925 <woloszym@sheridancollege.ca>
2022-02-22 08:35:12 -0500

    Button created

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
-    
-    
+    document.getElementById("school").innerHTML = "Sheridan College";
+    document.getElementById("myCampus").innerHTML = "Davis Campus";
 })
